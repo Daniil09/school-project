6cc04e025ebef69b966824ec3d9cef5df5589181
Daniil09 <76647136+Daniil09@users.noreply.github.com>
2024-03-09 03:09:01 +0500

    rgs

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
+let button = document.querySelector('#btn');
 
-function processForm() {
+button.addEventListener('click', () => {
     let sum = 0;
-    // Создаем массив
-    hc = new Array(15);
-    // Если обнаружено, что выбран первый вариант первого вопроса в форме, см. В документе, используйте сумму + оценка варианта
     if (document.see.c1[0].checked == 1) sum += 0;
     if (document.see.c1[1].checked == 1) sum += 500;
 
@@ -17,11 +15,30 @@ function processForm() {
     if (document.see.c3[2].checked == 1) sum += 155;
 
 
-    if (sum <= 4) document.see.answer.value = "Ваш счет" + sum + "Разум \n Вы психически здоровы и не в депрессии";
-    if (sum <= 10 && sum >= 5) document.see.answer.value = "Ваш счет" + sum + "Мин \n У вас иногда бывает депрессия";
-    if (sum <= 20 && sum >= 11) document.see.answer.value = "Ваш счет" + sum + "Мин \n У вас легкая депрессия";
-    if (sum <= 30 && sum >= 21) document.see.answer.value = "Ваш счет" + sum + "Мин \n У вас умеренная депрессия";
-    if (sum <= 45 && sum >= 31) document.see.answer.value = "Ваш счет" + sum + "Мин.\n У вас тяжелая депрессия, и вам нужно немедленно обратиться к специалисту";
+    if (sum = 15) document.see.answer.value = "1";
+    if (sum = 25) document.see.answer.value = "2";
+    if (sum = 50) document.see.answer.value = "3";
+    if (sum = 60) document.see.answer.value = "4";
+    if (sum = 65) document.see.answer.value = "5";
+    if (sum = 100) document.see.answer.value = "6";
+    if (sum = 115) document.see.answer.value = "7";
+    if (sum = 150) document.see.answer.value = "8";
+    if (sum = 155) document.see.answer.value = "9";
+    if (sum = 165) document.see.answer.value = "10";
+    if (sum = 205) document.see.answer.value = "11";
+    if (sum = 255) document.see.answer.value = "12";
+    if (sum = 515) document.see.answer.value = "13";
+    if (sum = 525) document.see.answer.value = "14";
+    if (sum = 550) document.see.answer.value = "15";
+    if (sum = 560) document.see.answer.value = "16";
+    if (sum = 565) document.see.answer.value = "17";
+    if (sum = 600) document.see.answer.value = "18";
+    if (sum = 615) document.see.answer.value = "19";
+    if (sum = 650) document.see.answer.value = "20";
+    if (sum = 655) document.see.answer.value = "21";
+    if (sum = 665) document.see.answer.value = "22";
+    if (sum = 705) document.see.answer.value = "23";
+    if (sum = 755) document.see.answer.value = "24";
 
-}
+})
 
